4a7f510f15ecca43c141a9c50176fdbcd6b3adb6
arafeek <adam_rafeek@hotmail.com>
2015-09-18 19:30:28 -0400

    Added support for importing files

--- src/components/editor/editor.controller.js
+++ src/components/editor/editor.controller.js
@@ -4,9 +4,10 @@ import ngsanitize from 'angular-sanitize';
 import {saveAs} from 'filesaver.js';
 
 export default class EditorController {
-  constructor($sanitize) {
+  constructor($timeout) {
     this.markdown = require('markdown').markdown;
     this.content = '';
+    this.$timeout = $timeout;
   }
 
   exportSource() {
@@ -16,6 +17,23 @@ export default class EditorController {
       });
 
       saveAs(data, this.fileName || 'note.md');
+    }
+  }
+
+  importFile() {
+    var self = this;
+    let file = document.getElementById('file-upload').files[0];
+    if (file) {
+      var fileReader = new FileReader();
+      fileReader.onloadend = (e) => {
+        this.$timeout(() => {
+          this.content = fileReader.result;
+          // Clear the input field
+          document.getElementById('file-upload').value = null;
+        }, 0) 
+      }
+      // Trigger the file read
+      fileReader.readAsText(file);
     }
   }
 }
