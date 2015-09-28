2188a190e61e9effbcd453832bcfbb465255e667
arafeek <adam_rafeek@hotmail.com>
2015-09-28 11:37:10 -0400

    Make heroku work

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm install webpack -g && npm run build; node server.js
+web: npm install webpack -g && npm run serve:dist
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+// Server used for heroku environment
+
 var express = require('express');
 var app = express();
 var path = require('path');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 var port = process.env.PORT || 8888;
-console.log('Magic happens on ' + port);
-app.listen(port);
+
+app.listen(port, function() {
+  console.log('Magic happens on port: ' + port);
+});
